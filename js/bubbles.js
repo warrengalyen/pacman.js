4502f00b10ed52897e89c259e21bfbd8d438f050
Warren Galyen <wgalyen@gmail.com>
2020-09-09 10:05:14 -0700

    Performance optimized + new controls for mobile

--- js/bubbles.js
+++ js/bubbles.js
@@ -8,11 +8,12 @@ var BUBBLES_Y_END = 522;
 var BUBBLES_SIZE = 3;
 var BUBBLES_COUNTER = 0;
 
+var SUPER_BUBBLES = [];
 var SUPER_BUBBLES_SIZE = 8;
 var SUPER_BUBBLES_BLINK = false;
 var SUPER_BUBBLES_BLINK_STATE = 0;
 var SUPER_BUBBLES_BLINK_TIMER = -1;
-var SUPER_BUBBLES_BLINK_SPEED = 250;
+var SUPER_BUBBLES_BLINK_SPEED = 300;
 
 function initBubbles() {
     BUBBLES_COUNTER = 0;
@@ -33,27 +34,29 @@ function drawBubbles() {
     var ctx = getBubblesCanvasContext();
     ctx.fillStyle = "#dca5be";
 
-    for (var line = 1, linemax = 29, i = 0; line <= linemax; line++) {
+    for (var line = 1, linemax = 29, i = 0, s = 0; line <= linemax; line ++) {
         var y = getYFromLine(line);
-        for (var x = BUBBLES_X_START, xmax = BUBBLES_X_END, bubble = 1; x < xmax; bubble++, x += BUBBLES_GAP) {
+        for (var x = BUBBLES_X_START, xmax = BUBBLES_X_END, bubble = 1 ; x < xmax; bubble ++, x += BUBBLES_GAP) {
             if (canAddBubble(line, bubble)) {
                 var type = "";
                 var size = "";
                 if (isSuperBubble(line, bubble)) {
                     type = "s";
                     size = SUPER_BUBBLES_SIZE;
+                    SUPER_BUBBLES[s] = line + ";" + bubble + ";" + correctionX(x, bubble) + "," + y;
+                    s ++;
                 } else {
                     type = "b";
                     size = BUBBLES_SIZE;
                 }
-                BUBBLES_COUNTER++;
+                BUBBLES_COUNTER ++;
                 ctx.beginPath();
-                ctx.arc(x, y, size, 0, 2 * Math.PI, false);
+                ctx.arc(correctionX(x, bubble), y, size, 0, 2 * Math.PI, false);
                 ctx.fill();
                 ctx.closePath();
 
-                BUBBLES[i] = line + ";" + bubble + ";" + x + "," + y + ";" + type + ";0"
-                i++;
+                BUBBLES[parseInt(correctionX(x, bubble)) + "," + parseInt(y)] = line + ";" + bubble + ";" + type + ";0"
+                i ++;
             }
         }
     }
@@ -77,25 +80,24 @@ function blinkSuperBubbles() {
             SUPER_BUBBLES_BLINK_STATE = 0;
         }
 
-        for (var i = 0, imax = BUBBLES.length; i < imax; i++) {
-            var b = BUBBLES[i];
+        for (var i = 0, imax = SUPER_BUBBLES.length; i < imax; i ++) {
 
-            var line = b.split(";")[0];
-            var bubble = b.split(";")[1];
-            var x = parseInt(b.split(";")[2].split(",")[0]);
-            var y = parseInt(b.split(";")[2].split(",")[1]);
-            var type = b.split(";")[3];
-            var eat = b.split(";")[4];
+            var s = SUPER_BUBBLES[i];
+            var sx = parseInt(s.split(";")[2].split(",")[0]);
+            var sy = parseInt(s.split(";")[2].split(",")[1]);
 
-            if (type === "s" && eat === "0") {
+            var b = BUBBLES[sx + "," + sy];
+            var eat = b.split(";")[3];
+
+            if (eat === "0") {
 
                 if (SUPER_BUBBLES_BLINK_STATE === 1) {
-                    eraseBubble("s", x, y);
+                    eraseBubble("s", sx, sy);
                 } else {
                     var ctx = getBubblesCanvasContext();
                     ctx.fillStyle = "#dca5be";
                     ctx.beginPath();
-                    ctx.arc(x, y, SUPER_BUBBLES_SIZE, 0, 2 * Math.PI, false);
+                    ctx.arc(sx, sy, SUPER_BUBBLES_SIZE, 0, 2 * Math.PI, false);
                     ctx.fill();
                     ctx.closePath();
                 }
@@ -109,9 +111,6 @@ function eraseBubble(t, x, y) {
 
     var ctx = getBubblesCanvasContext();
 
-    // ctx.save();
-    // ctx.globalCompositeOperation = "destination-out";
-
     var size = "";
     if (t === "s") {
         size = SUPER_BUBBLES_SIZE;
@@ -119,16 +118,11 @@ function eraseBubble(t, x, y) {
         size = BUBBLES_SIZE;
     }
 
-    //ctx.beginPath();
     ctx.clearRect(x - size, y - size, (size + 1) * 2, (size + 1) * 2);
-    //ctx.fill();
-    //ctx.closePath();
-
-    // ctx.restore();
 }
 
 function isSuperBubble(line, bubble) {
-    if ((line === 23 || line === 4) && (bubble === 1 || bubble === 26)) {
+    if ( (line === 23 || line === 4) && (bubble === 1 || bubble === 26)) {
         return true;
     }
 
@@ -137,57 +131,77 @@ function isSuperBubble(line, bubble) {
 
 function canAddBubble(line, bubble) {
 
-    if (((line >= 1 && line <= 4) || (line >= 9 && line <= 10) || (line >= 20 && line <= 22) || (line >= 26 && line <= 28)) && (bubble === 13 || bubble === 14)) {
+    if ( ( ( line >= 1 && line <= 4) || (line >= 9 && line <= 10) || (line >= 20 && line <= 22) || (line >= 26 && line <= 28) ) && (bubble === 13 || bubble === 14)) {
         return false;
-    } else if (((line >= 2 && line <= 4) || (line >= 21 && line <= 22)) && ((bubble >= 2 && bubble <= 5) || (bubble >= 7 && bubble <= 11) || (bubble >= 16 && bubble <= 20) || (bubble >= 22 && bubble <= 25))) {
+    } else if ( ( (line >= 2 && line <= 4) || (line >= 21 && line <= 22) ) && ( (bubble >= 2 && bubble <= 5) || (bubble >= 7 && bubble <= 11) || (bubble >= 16 && bubble <= 20) || (bubble >= 22 && bubble <= 25) ) ) {
         return false;
-    } else if ((line >= 6 && line <= 7) && ((bubble >= 2 && bubble <= 5) || (bubble >= 7 && bubble <= 8) || (bubble >= 10 && bubble <= 17) || (bubble >= 19 && bubble <= 20) || (bubble >= 22 && bubble <= 25))) {
+    } else if ( ( line >= 6 && line <= 7 ) && ( (bubble >= 2 && bubble <= 5) || (bubble >= 7 && bubble <= 8) || (bubble >= 10 && bubble <= 17) ||  (bubble >= 19 && bubble <= 20) || (bubble >= 22 && bubble <= 25) ) ) {
         return false;
-    } else if ((line === 8) && ((bubble >= 7 && bubble <= 8) || (bubble >= 13 && bubble <= 14) || (bubble >= 19 && bubble <= 20))) {
+    } else if ( ( line === 8 ) && ( (bubble >= 7 && bubble <= 8) || (bubble >= 13 && bubble <= 14) || (bubble >= 19 && bubble <= 20) ) ) {
         return false;
-    } else if (((line >= 9 && line <= 19)) && ((bubble >= 1 && bubble <= 5) || (bubble >= 22 && bubble <= 26))) {
+    } else if ( (( line >= 9 &&  line <= 19) ) && ( (bubble >= 1 && bubble <= 5) || (bubble >= 22 && bubble <= 26) ) ) {
         return false;
-    } else if ((line === 11 || line === 17) && ((bubble >= 7 && bubble <= 20))) {
+    } else if ( ( line === 11 || line === 17 ) && ( (bubble >= 7 && bubble <= 20) ) ) {
         return false;
-    } else if ((line === 9 || line === 10) && ((bubble === 12 || bubble === 15))) {
+    } else if ( ( line === 9 || line === 10 ) && ( (bubble === 12 || bubble === 15) ) ) {
         return false;
-    } else if (((line >= 12 && line <= 13) || (line >= 15 && line <= 16)) && ((bubble === 9 || bubble === 18))) {
+    } else if ( ( (line >= 12 && line <= 13) || (line >= 15 && line <= 16) ) && ( (bubble === 9 || bubble === 18) ) ) {
         return false;
-    } else if (line === 14 && ((bubble >= 7 && bubble <= 9) || (bubble >= 18 && bubble <= 20))) {
+    } else if ( line === 14 && ( (bubble >= 7 && bubble <= 9) || (bubble >= 18 && bubble <= 20) ) ) {
         return false;
-    } else if ((line === 18 || line === 19) && (bubble === 9 || bubble === 18)) {
+    } else if ( (line === 18 || line === 19) && ( bubble === 9 || bubble === 18) ) {
         return false;
-    } else if ((line >= 9 && line <= 10) && ((bubble >= 7 && bubble <= 11) || (bubble >= 16 && bubble <= 20))) {
+    } else if ( ( line >= 9 && line <= 10 ) && ( (bubble >= 7 && bubble <= 11) || (bubble >= 16 && bubble <= 20) ) ) {
         return false;
-    } else if (((line >= 11 && line <= 13) || (line >= 15 && line <= 19)) && ((bubble >= 7 && bubble <= 8) || (bubble >= 19 && bubble <= 20))) {
+    } else if ( (( line >= 11 && line <= 13) || (line >= 15 && line <= 19) ) && ( (bubble >= 7 && bubble <= 8) || (bubble >= 19 && bubble <= 20) ) ) {
         return false;
-    } else if (((line >= 12 && line <= 16) || (line >= 18 && line <= 19)) && (bubble >= 10 && bubble <= 17)) {
+    } else if ( ( (line >= 12 && line <= 16) || (line >= 18 && line <= 19) ) && ( bubble >= 10 && bubble <= 17) ) {
         return false;
-    } else if ((line === 23) && ((bubble >= 4 && bubble <= 5) || (bubble >= 22 && bubble <= 23))) {
+    } else if ( (line === 23) && ( (bubble >= 4 && bubble <= 5) || (bubble >= 22 && bubble <= 23) ) ) {
         return false;
-    } else if ((line >= 24 && line <= 25) && ((bubble >= 1 && bubble <= 2) || (bubble >= 4 && bubble <= 5) || (bubble >= 7 && bubble <= 8) || (bubble >= 10 && bubble <= 17) || (bubble >= 19 && bubble <= 20) || (bubble >= 22 && bubble <= 23) || (bubble >= 25 && bubble <= 26))) {
+    } else if ( (line >= 24 && line <= 25) && ( (bubble >= 1 && bubble <= 2) || (bubble >= 4 && bubble <= 5) || (bubble >= 7 && bubble <= 8) || (bubble >= 10 && bubble <= 17) || (bubble >= 19 && bubble <= 20) || (bubble >= 22 && bubble <= 23) || (bubble >= 25 && bubble <= 26) ) ) {
         return false;
-    } else if ((line === 26) && ((bubble >= 7 && bubble <= 8) || (bubble >= 19 && bubble <= 20))) {
+    } else if ( (line === 26) && ( (bubble >= 7 && bubble <= 8) || (bubble >= 19 && bubble <= 20) ) ) {
         return false;
-    } else if ((line >= 27 && line <= 28) && ((bubble >= 2 && bubble <= 11) || (bubble >= 16 && bubble <= 25))) {
+    } else if ( (line >= 27 && line <= 28) && ( (bubble >= 2 && bubble <= 11) || (bubble >= 16 && bubble <= 25) ) ) {
         return false;
     }
 
     return true;
 }
 
+function correctionX(x, bubble) {
+
+    if (bubble === 3) {
+        return x + 1;
+    } else if (bubble === 6) {
+        return x + 1;
+    } else if (bubble === 15) {
+        return x + 1;
+    } else if (bubble === 18) {
+        return x + 1;
+    } else if (bubble === 21) {
+        return x + 2;
+    } else if (bubble === 24) {
+        return x + 2;
+    } else if (bubble === 26) {
+        return x + 1;
+    }
+    return x;
+}
+
 function getYFromLine(line) {
     var y = BUBBLES_Y_START;
     if (line < 8) {
-        y = BUBBLES_Y_START + ((line - 1) * 18);
+        y = BUBBLES_Y_START + ( (line - 1) * 18 );
     } else if (line > 7 && line < 15) {
-        y = 150 + ((line - 8) * 18);
+        y = 150 + ( (line - 8) * 18 );
     } else if (line > 14 && line < 21) {
-        y = 256 + ((line - 14) * 18);
+        y = 256 + ( (line - 14) * 18 );
     } else if (line > 20 && line < 26) {
-        y = 362 + ((line - 20) * 18);
+        y = 362 + ( (line - 20) * 18 );
     } else if (line > 25 && line < 29) {
-        y = 451 + ((line - 25) * 18);
+        y = 452 + ( (line - 25) * 18 );
     } else if (line === 29) {
         y = BUBBLES_Y_END;
     }
